411d3eabb71e3f5b945f902c5208dfff06a38d0f
Bhavya <bhavya.sachdeva.58@gmail.com>
2022-01-20 22:44:25 +0530

    Added

--- client/src/components/TeamChannelList.js
+++ client/src/components/TeamChannelList.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { AddChannel } from '../assets';
 
-const TeamChannelList = () => {
+ const TeamChannelList = ({ children, error = false, loading, type}) => {
+       if(error) {
+             return type === 'team' ? (
+                    <div className="team-channel-list">
+                         <p className="team-channel-list__message">
+                          </p>
+                    </div>
+             ) : null
+        }
+
+        if (loading) {
+               return (
+                   <div className="team-channel-list">
+                         <p className="team-channel-list__message loading">
+                               {type=== 'team' ? 'channels' : 'Messages' } loading ...
+                          </p>
+                    </div>
+               )
+        }
+
+
     return (
-        <div>
-            
+        <div className="team-channel-list">
+              <div className="team-channel-list__header">
+                   <p className="team-channel-list__header__title">
+                           {type=== 'team' ? 'channels' : 'Direct Messages' } 
+                   </p>
+              </div>
+              {children}
         </div>
     )
 }
 
-export default TeamChannelList
+export default TeamChannelList;
